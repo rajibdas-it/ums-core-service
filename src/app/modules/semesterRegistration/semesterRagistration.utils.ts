e713bd1143c96cb8d207d52f24a70cb0e2f202b8
rajibdas-it <rajibrad@gmail.com>
2023-11-17 12:55:49 +0600

    available courselist done

--- src/app/modules/semesterRegistration/semesterRagistration.utils.ts
+++ src/app/modules/semesterRegistration/semesterRagistration.utils.ts
@@ -8,25 +8,49 @@ const getAvailableCourses = (
     (course: any) => course.courseId,
   );
 
-  const availableCourseList = offeredCourses
+  // available course jegula ami complete korsi oi badh e
+
+  const availableCoursesList = offeredCourses
     .filter(
       (offeredCourse: any) => !completedCourseId.includes(offeredCourse.id),
     )
     .filter((course: any) => {
       const preRequisites = course.course.preRequisite;
-      if (preRequisite.length === 0) {
+      if (preRequisites.length === 0) {
         return true;
       } else {
-        const preRequisite = preRequisites.map(
-          (preRequisites: any) => preRequisites.preRequisiteId,
-          console.log('pre-requite ', preRequisites),
+        const preRequisiteIds = preRequisites.map(
+          (preRequisite: any) => preRequisite?.preRequisiteId,
         );
-
-        return;
+        return preRequisiteIds.every((id: string) =>
+          completedCourseId.includes(id),
+        );
+      }
+    })
+    .map((course: any) => {
+      const isAlreadyTakenCourse = studentCurrentlyCourses.find(
+        (c: any) => c.offeredCourseId === course.id,
+      );
+      if (isAlreadyTakenCourse) {
+        course.offeredCourseSections.map((section: any) => {
+          if (section.id === isAlreadyTakenCourse.offeredCourseSectionId) {
+            section.isTaken = true;
+          } else {
+            section.isTaken = false;
+          }
+        });
+        return { ...course, isTaken: true };
+      } else {
+        course.offeredCourseSections.map((section: any) => {
+          section.isTaken = false;
+        });
+        return {
+          ...course,
+          isTaken: false,
+        };
       }
     });
-
-  return null;
+  return availableCoursesList;
 };
 
 export const semesterRegistrationUtils = {
