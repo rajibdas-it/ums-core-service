829372282f02f1aa3544589124317a30db9f8ba3
rajibdas-it <rajibrad@gmail.com>
2023-11-22 17:17:13 +0600

    listen from redis

--- src/app/modules/student/student.events.ts
+++ src/app/modules/student/student.events.ts
@@ -1,10 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { RedisClient } from '../../../shared/redis';
 import { Event_Student_Created } from './student.constant';
 
 const userEvents = () => {
   RedisClient.subscribe(Event_Student_Created, async e => {
     const data = JSON.parse(e);
-    console.log(data);
   });
 };
 
